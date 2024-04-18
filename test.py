14dcfe53a61bb4584945b4323e039488f3fefa1f
sdh <shan_de_heng@163.com>
2024-04-18 15:41:33 +0800

    222222222222

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 import numpy as np
 print('hello')
+for i in range(1,100):
+    for j in range(1,10):
+        if(i<=j):
+            print(i*j)
+        else:
+            print(i-j)
+             
+                     
